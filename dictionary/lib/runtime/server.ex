ced04f980c045177f668a50d0c19709578a8d54d
David Ibokette <114680837+david-ibokette@users.noreply.github.com>
2024-07-25 16:32:33 -0400

    Dictionary started by a Supervisor;

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -3,11 +3,17 @@ defmodule Dictionary.Runtime.Server do
 
   @me __MODULE__
 
-  def start_link do
+  use Agent
+
+  def start_link(_args) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word do
+    if :rand.uniform < 0.13 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(@me, &WordList.random_word/1)
   end
 end
