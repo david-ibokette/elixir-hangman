d728e196cea5eab07a98342acbf0c123d027d4a7
David Ibokette <114680837+david-ibokette@users.noreply.github.com>
2024-09-17 09:35:08 -0400

    More Tidying up;

--- b1/lib/b1_web/controllers/hangman_html.ex
+++ b1/lib/b1_web/controllers/hangman_html.ex
@@ -8,6 +8,39 @@ defmodule B1Web.HangmanHTML do
 
   embed_templates "hangman_html/*"
 
+  def continue_or_try_again(assigns) do
+    ~H"""
+  <.link :if={@state in [:won, :lost]} href={~p"/hangman"}>
+	<.button>Try again</.button>
+</.link>
+<.form
+	:let={f}
+	:if={@state not in [:won, :lost]}
+	for={%{}}
+  	as={:make_move}
+  	action={~p"/hangman"}
+  	method="put"
+>
+	<.input field={f[:guess]} />
+	<.button>Make next guess</.button>
+</.form>
+"""
+  end
+
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, a letter a a time" },
+    good_guess:   {"good-guess",    "Good guess!"},
+    bad_guess:    {"bad-guess",     "Sorry, that's a bad guess"},
+    won:          {"won",           "You won!"},
+    lost:         {"lost",          "Sorry, you lost"},
+    already_used: {"already-used",  "You already used that letter"}
+  }
+
+  def move_status(status) do
+    { class, msg } = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
   def figure_for(0) do
     ~s{
   +---+
