68caebddcf1f880089c9ebb05764f34206ab4459
David Ibokette <114680837+david-ibokette@users.noreply.github.com>
2024-09-19 09:11:53 -0400

    Added a watcher!

--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -1,8 +1,11 @@
 defmodule Hangman.Runtime.Server do
   alias Hangman.Impl.Game
+  alias Hangman.Runtime.Watchdog
+
   use GenServer
 
   @type t :: pid
+  @idle_timeout 1 * 60 * 60 * 1000   # 1 hour
 
   ### client process code
 
@@ -13,16 +16,19 @@ defmodule Hangman.Runtime.Server do
   ### server process code
 
   def init(_) do
-    { :ok, Game.new_game }
+    watcher = Watchdog.start(@idle_timeout)
+    { :ok, { Game.new_game, watcher} }
   end
 
-  def handle_call({:make_move, guess}, _from, game) do
+  def handle_call({:make_move, guess}, _from, { game, watcher }) do
+    Watchdog.im_alive(watcher)
     { updated_game, tally } = Game.make_move(game, guess)
-    { :reply, tally, updated_game }
+    { :reply, tally, { updated_game, watcher } }
   end
 
-  def handle_call({:tally}, _from, game) do
-    { :reply, Game.tally(game), game }
+  def handle_call({:tally}, _from, { game, watcher }) do
+    Watchdog.im_alive(watcher)
+    { :reply, Game.tally(game), { game, watcher } }
   end
 
 end
